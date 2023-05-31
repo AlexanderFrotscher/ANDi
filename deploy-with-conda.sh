d5d4ce7b4e73205f441e607f08f5b59f6b06f268
AlexanderFrotscher <alex.frotscher@gmx.de>
2023-05-31 10:38:24 +0200

    deploy script

--- deploy-with-conda.sh
+++ deploy-with-conda.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
+#SBATCH --job-name=BraTS-Diffusion
 #SBATCH --ntasks=1                # Number of tasks (see below)
-#SBATCH --cpus-per-task=32         # Number of CPU cores per task
+#SBATCH --cpus-per-task=16         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
 #SBATCH --time=1-10:00            # Runtime in D-HH:MM
 #SBATCH --partition=a100 # Partition to submit to
 #SBATCH --gres=gpu:8              # optionally type and number of gpus
-#SBATCH --mem=50G                 # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=logs/job_%j.out  # File to which STDOUT will be written
-#SBATCH --error=logs/job_%j.err   # File to which STDERR will be written
-#SBATCH --mail-type=FAIL           # Type of email notification- BEGIN,END,FAIL,ALL
-#SBATCH --mail-user=alexander.frotscher@student.uni-tuebingen.de  # Email to which notifications will be sent
+#SBATCH --mem=64G                 # Memory pool for all cores (see also --mem-per-cpu)
 
 # print info about current job
 echo "---------- JOB INFOS ------------"
-scontrol show job $SLURM_JOB_ID 
+scontrol show job $SLURM_JOB_ID
 echo -e "---------------------------------\n"
 
 # Due to a potential bug, we need to manually load our bash configurations first
 source $HOME/.bashrc
 
-# Next activate the conda environment 
+# Next activate the conda environment
 conda activate myenv
 
 # Run our code
 echo "-------- PYTHON OUTPUT ----------"
-accelerate launch diffusion_cfg.py 
+srun --gres=gpu:8 -n 1 -N 1 -c 16 --mem=64G --partition=a100 --time=1-10:00 accelerate launch diffusion_cfg.py
 echo "---------------------------------"
 
 # Deactivate environment again
