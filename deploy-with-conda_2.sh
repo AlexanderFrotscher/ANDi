ba6bdb4b298cc90b7da2f5a0f5ddbfba76726e13
AlexanderFro <alex.frotscher@gmx.de>
2023-07-04 10:02:39 +0200

    eval deploy

--- deploy-with-conda_2.sh
+++ deploy-with-conda_2.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=16         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-01:30            # Runtime in D-HH:MM
+#SBATCH --time=0-07:30            # Runtime in D-HH:MM
 #SBATCH --partition=a100 # Partition to submit to
 #SBATCH --gres=gpu:4              # optionally type and number of gpus
 #SBATCH --mem=200GB                 # Memory pool for all cores (see also --mem-per-cpu)
@@ -21,7 +21,7 @@ conda activate myenv
 
 # Run our code
 echo "-------- PYTHON OUTPUT ----------"
-srun -n 1 -N 1 -c 16 --mem=200GB --partition=a100 --time=0-01:30 --gres=gpu:4 accelerate launch eval.py
+srun -n 1 -N 1 -c 16 --mem=200GB --partition=a100 --time=0-07:30 --gres=gpu:4 accelerate launch eval.py
 echo "---------------------------------"
 
 # Deactivate environment again
