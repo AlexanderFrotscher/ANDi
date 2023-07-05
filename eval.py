9b939c9c6c7391943212d8ef7d5ab17df837cb56
AlexanderFro <alex.frotscher@gmx.de>
2023-07-05 16:27:06 +0200

    new method

--- eval.py
+++ eval.py
@@ -21,7 +21,7 @@ def main():
     args.dataset_path = "/mnt/lustre/baumgartner/bkc035/data/BraTS2021/BraTS2021_Training_Data"
     #args.dataset_path = './data/BraTS20'
     args.path_to_csv = "/mnt/lustre/baumgartner/bkc035/data/BraTS2021/BraTS2021_Training_Data/tumor_slices_val.csv"
-    #args.path_to_csv = './data/BraTS20/tumor_slices_test.csv'
+    #args.path_to_csv = './data/BraTS20/tumor_slices_small.csv'
     args.batch_size = 16
     args.image_size = 64
 
@@ -37,85 +37,80 @@ def main():
 
     model, dataloader = accelerator.prepare(model, dataloader)
     pbar = tqdm(dataloader)
-    threshold_diff = [x / 100 for x in range(1, 100)]
-    threshold_test = [round(x,3) for x in np.arange(1.0,1.6,0.1)]
-    threshold_test_1 = [round(x, 3) for x in np.arange(1.0, 1.6, 0.1)]
-    threshold_test_2 = [round(-x, 3) for x in np.arange(2.2, 2.8, 0.1)]
-    threshold_test_3 = [round(x, 3) for x in np.arange(3, 3.5, 0.1)]
-    threshold_test_3_m = [round(-x, 3) for x in np.arange(2.7, 3.3, 0.1)]
-    threshold_test_4 = [round(x, 3) for x in np.arange(2.0, 2.5, 0.1)]
-    my_thresholds = list(itertools.product(threshold_test_1,threshold_test_2,threshold_test_3,threshold_test_3_m,threshold_test_4))
+    #threshold_diff = [x / 100 for x in range(1, 100)]
+    threshold_test = [round(x,3) for x in np.arange(0.7,2.2,0.1)]
+    #threshold_test_1 = [round(x, 3) for x in np.arange(1.0, 1.6, 0.1)]
+    #threshold_test_2 = [round(-x, 3) for x in np.arange(2.2, 2.8, 0.1)]
+    #threshold_test_3 = [round(x, 3) for x in np.arange(3.0, 3.5, 0.1)]
+    #threshold_test_3_m = [round(-x, 3) for x in np.arange(2.7, 3.3, 0.1)]
+    #threshold_test_4 = [round(x, 3) for x in np.arange(2.3, 2.8, 0.1)]
+    #threshold_test_1 = [round(x, 3) for x in np.arange(0.9, 1.5, 0.1)]
+    #threshold_test_2 = [round(-x, 3) for x in np.arange(1.4, 2.3, 0.1)]
+    #threshold_test_3 = [round(x, 3) for x in np.arange(1.7, 2.3, 0.1)]
+    #threshold_test_3_m = [round(-x, 3) for x in np.arange(1.0, 1.7, 0.1)]
+    #threshold_test_4 = [round(x, 3) for x in np.arange(0.9, 1.5, 0.1)]
+    #my_thresholds = list(itertools.product(threshold_test_1,threshold_test_2,threshold_test_3,threshold_test_3_m,threshold_test_4))
 
-    dice_scores_diff = {i: [] for i in threshold_diff}
-    dice_scores_diff_2 = {i: [] for i in threshold_diff}
-    dice_scores_diff_3 = {i: [] for i in threshold_diff}
+    #dice_scores_diff = {i: [] for i in threshold_diff}
+    #dice_scores_diff_2 = {i: [] for i in threshold_diff}
     dice_scores_mask = {i: [] for i in threshold_test}
-    dice_scores_mask_3 = {(a,b,c,d,e):[] for (a,b,c,d,e) in my_thresholds}
+    #dice_scores_mask_3 = {(a,b,c,d,e):[] for (a,b,c,d,e) in my_thresholds}
     my_resize = transforms.Resize(128, antialias=True)
     #my_transform = transforms.Lambda(lambda x: (x * 2) - 1)
-    #scaling = diffusion.alpha_hat
+    scaling = torch.linspace(0.4,1.0,1000).to(device)
+    my_scaling = diffusion.beta * scaling
+    my_scaling = my_scaling.to(device)
     for i, (image, label) in enumerate(pbar):
         #image = my_transform(image).to(device)
         label = label.to(device)
         label = label[:, 0, :, :].type(torch.uint8)
         num_steps = 1000
-        xts, zs = diffusion.dpm_inversion(model, image,timestemp=num_steps)
-        #my_mean = torch.abs(torch.mean(zs,dim=1))
-        #plot_images(my_mean, mode='L')
-        """
-        my_images_one = diffusion.guide_restoration(
-            model, xts[:, 0:250], zs[:, 0:250], cfg_scale=0, noise_scale=0.3
-        )
-        my_images_two = diffusion.guide_restoration(
-            model, xts[:, 0:150], zs[:, 0:150], cfg_scale=0, noise_scale=0.4
-        )
-        my_images_three = diffusion.guide_restoration(
-            model, xts[:, 0:200], zs[:, 0:200], cfg_scale=0, noise_scale=0.5
-        )
-        
+        xts, zs = diffusion.dpm_inversion(model, image,timestemp=num_steps, scaling=my_scaling)
+        #my_mean = torch.mean(zs,dim=1) * np.sqrt(1000)
+        #my_mean_1 =  my_mean[:,0]
+        #my_mean_2 =  my_mean[:,3]
+        #my_mean = (my_mean_1 + my_mean_2) * 0.5
+        #show_slices([my_mean[0].cpu(),my_mean[1].cpu(),my_mean[2].cpu()])
+        #my_images_one = diffusion.guide_restoration(
+        #    model, xts[:, 0:150], zs[:, 0:150], cfg_scale=0, noise_scale=0.45
+        #)
 
-        for key in dice_scores_diff:
-            my_masks = create_difference(image, my_images_one, threshold=key)
-            my_masks = my_resize(my_masks)
-            dice_scores_diff[key].extend([float(x) for x in dice(my_masks, label)])
-            my_masks = create_difference(image, my_images_two, threshold=key)
-            my_masks = my_resize(my_masks)
-            dice_scores_diff_2[key].extend([float(x) for x in dice(my_masks, label)])
-            my_masks = create_difference(image, my_images_three, threshold=key)
-            my_masks = my_resize(my_masks)
-            dice_scores_diff_3[key].extend([float(x) for x in dice(my_masks, label)])
-        """
+
+        #for key in dice_scores_diff:
+        #    my_masks = create_difference(image, my_images_one, threshold=key)
+        #    my_masks = my_resize(my_masks)
+        #    dice_scores_diff[key].extend([float(x) for x in dice(my_masks, label)])
+            #my_masks = create_difference(image, my_images_two, threshold=key)
+            #my_masks = my_resize(my_masks)
+            #dice_scores_diff_2[key].extend([float(x) for x in dice(my_masks, label)])
+
         for key in dice_scores_mask:
-            mask = create_mask_2(zs, key,steps=num_steps-1)
+            mask = create_mask_2(zs, key,steps=num_steps)
             mask = my_resize(mask)
             dice_scores_mask[key].extend([float(x) for x in dice(mask, label)])
-        for key in dice_scores_mask_3:
-            mask = create_mask_3(zs,key,steps=num_steps-1)
-            mask = my_resize(mask)
-            dice_scores_mask_3[key].extend([float(x) for x in dice(mask, label)])
+        #for key in dice_scores_mask_3:
+        #    mask = create_mask_3(zs,key,steps=num_steps)
+        #    mask = my_resize(mask)
+        #    dice_scores_mask_3[key].extend([float(x) for x in dice(mask, label)])
     #for key in dice_scores_diff:
     #    dice_scores_diff[key] = np.mean(np.asarray(dice_scores_diff[key]))
     #    dice_scores_diff_2[key] = np.mean(np.asarray(dice_scores_diff_2[key]))
-    #    dice_scores_diff_3[key] = np.mean(np.asarray(dice_scores_diff_3[key]))
     for key in dice_scores_mask:
         dice_scores_mask[key] = np.mean(np.asarray(dice_scores_mask[key]))
-    for key in dice_scores_mask_3:
-        dice_scores_mask_3[key] = np.mean(np.asarray(dice_scores_mask_3[key]))
+    #for key in dice_scores_mask_3:
+    #    dice_scores_mask_3[key] = np.mean(np.asarray(dice_scores_mask_3[key]))
     #df_diff = pd.DataFrame(dice_scores_diff, index=[0]).T
     #df_diff.index.rename("threshold", inplace=True)
     #df_diff_2 = pd.DataFrame(dice_scores_diff_2, index=[0]).T
     #df_diff_2.index.rename("threshold", inplace=True)
-    #df_diff_3 = pd.DataFrame(dice_scores_diff_3, index=[0]).T
-    #df_diff_3.index.rename("threshold", inplace=True)
     #df = pd.concat([df_diff,df_diff_2],axis=1)
-    #df = pd.concat([df,df_diff_3],axis=1)
-    #df.columns = ['0.3 - 250','0.4 - 150','0.5 - 200']
+    #df.columns = ['0.3 - 150','0.05 - 1000']
     df_mask = pd.DataFrame(dice_scores_mask, index=[0]).T
-    df_mask2 = pd.DataFrame(dice_scores_mask_3, index=[0]).T
+    #df_mask2 = pd.DataFrame(dice_scores_mask_3, index=[0]).T
     #df.to_csv("/mnt/lustre/baumgartner/bkc035/data/BraTS2021/BraTS2021_Training_Data/difference_score.csv")
     df_mask.to_csv("/mnt/lustre/baumgartner/bkc035/data/BraTS2021/BraTS2021_Training_Data/mask_one.csv")
-    df_mask2.to_csv("/mnt/lustre/baumgartner/bkc035/data/BraTS2021/BraTS2021_Training_Data/mask_two.csv")
-    #df.to_csv("./data/BraTS20/difference_score.csv")
+    #df_mask2.to_csv("/mnt/lustre/baumgartner/bkc035/data/BraTS2021/BraTS2021_Training_Data/mask_two.csv")
+    #df_diff.to_csv("./results/BraTS21/difference_score.csv")
     #df_mask.to_csv("./results/BraTS21/mask_one.csv")
     #df_mask2.to_csv("./results/BraTS21/mask_two.csv")
     """
@@ -146,10 +141,13 @@ def show_slices(slices):
     plt.show()
 
 def create_mask_2(zs,th,steps):
-    my_mean = torch.mean(zs,dim=1) * np.sqrt(steps-1)
-    #my_mean = torch.mean(zs,dim=1) * 100
-    my_mean[:,0] = torch.where(my_mean[:,0]>th,1,0)
-    return my_mean[:,0].type(torch.uint8)
+    my_mean = torch.mean(zs,dim=1) * np.sqrt(steps)
+    my_mean_1 =  my_mean[:,0]
+    my_mean_2 =  my_mean[:,3]
+    my_mean = (my_mean_1 + my_mean_2) * 0.5
+    my_mean[my_mean < th] = 0
+    my_mean[my_mean != 0] = 1
+    return my_mean.type(torch.uint8)
 
 def create_mask_3(zs,my_th, steps):
     th_m1 = my_th[0]
@@ -157,7 +155,7 @@ def create_mask_3(zs,my_th, steps):
     th_m3 = my_th[2]
     th_m3_m = my_th[3]
     th_m4 = my_th[4]
-    my_mean = torch.mean(zs,dim=1) * np.sqrt(steps-1)
+    my_mean = torch.mean(zs,dim=1) * np.sqrt(steps)
     my_mean[:,0] = torch.where(my_mean[:,0]>th_m1,1.0,0.0)
     my_mean[:,1] = torch.where(my_mean[:,1]<th_m2,1.0,0.0)
     mask_3_p = torch.where(my_mean[:,2]>th_m3,1.0,0.0)
@@ -167,7 +165,7 @@ def create_mask_3(zs,my_th, steps):
     my_mean[:,2] = mask_3
     my_mean[:,3] = torch.where(my_mean[:,3]>th_m4,1.0,0.0)
     my_mean = torch.mean(my_mean,dim=1)
-    my_mean[my_mean>0] = 1
+    my_mean[my_mean>0.25] = 1
     return my_mean
 
 
