acb3bbd2b2d1138e9f84cfdc462d699592e5f4e2
AlexanderFrotscher <alex.frotscher@gmx.de>
2023-08-07 11:25:37 +0200

    jobs reqs

--- deploy-with-conda_2.sh
+++ deploy-with-conda_2.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=0-07:30            # Runtime in D-HH:MM
 #SBATCH --partition=a100 # Partition to submit to
 #SBATCH --gres=gpu:1              # optionally type and number of gpus
-#SBATCH --mem=32GB                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=16GB                 # Memory pool for all cores (see also --mem-per-cpu)
 
 # print info about current job
 echo "---------- JOB INFOS ------------"
@@ -21,7 +21,7 @@ conda activate myenv
 
 # Run our code
 echo "-------- PYTHON OUTPUT ----------"
-srun -n 1 -N 1 -c 8 --mem=32GB --partition=a100 --time=0-07:30 --gres=gpu:1 accelerate launch eval_3d.py
+srun -n 1 -N 1 -c 8 --mem=16GB --partition=a100 --time=0-07:30 --gres=gpu:1 accelerate launch eval_3d.py
 echo "---------------------------------"
 
 # Deactivate environment again
--- deploy-with-conda_4.sh
+++ deploy-with-conda_4.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=0-10:30            # Runtime in D-HH:MM
 #SBATCH --partition=a100 # Partition to submit to
 #SBATCH --gres=gpu:1              # optionally type and number of gpus
-#SBATCH --mem=64GB                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=16GB                 # Memory pool for all cores (see also --mem-per-cpu)
 
 # print info about current job
 echo "---------- JOB INFOS ------------"
@@ -21,7 +21,7 @@ conda activate myenv
 
 # Run our code
 echo "-------- PYTHON OUTPUT ----------"
-srun -n 1 -N 1 -c 8 --mem=64GB --partition=a100 --time=0-10:30 --gres=gpu:1 accelerate launch gausian_mixture_eval.py
+srun -n 1 -N 1 -c 8 --mem=16GB --partition=a100 --time=0-10:30 --gres=gpu:1 accelerate launch gausian_mixture_eval.py
 echo "---------------------------------"
 
 # Deactivate environment again
