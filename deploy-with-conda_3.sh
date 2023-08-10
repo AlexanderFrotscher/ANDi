9659428ef738da3b1968f1765697f401d8aee9c3
AlexanderFrotscher <alex.frotscher@gmx.de>
2023-08-10 15:38:34 +0200

    thr resc

--- deploy-with-conda_3.sh
+++ deploy-with-conda_3.sh
@@ -3,10 +3,8 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=8         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-07:30            # Runtime in D-HH:MM
-#SBATCH --partition=a100 # Partition to submit to
-#SBATCH --gres=gpu:1              # optionally type and number of gpus
-#SBATCH --mem=4GB                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --time=0-01:00            # Runtime in D-HH:MM
+#SBATCH --mem=32GB                 # Memory pool for all cores (see also --mem-per-cpu)
 
 # print info about current job
 echo "---------- JOB INFOS ------------"
@@ -21,7 +19,7 @@ conda activate myenv
 
 # Run our code
 echo "-------- PYTHON OUTPUT ----------"
-srun -n 1 -N 1 -c 8 --mem=4GB --partition=a100 --time=0-07:30 --gres=gpu:1 accelerate launch thresholding.py
+srun -n 1 -N 1 -c 8 --mem=32GB --time=0-01:00 python thresholding.py
 echo "---------------------------------"
 
 # Deactivate environment again
