e5ad74dfde7629febdda1cc49e4c59a388bb6180
AlexanderFro <alex.frotscher@gmx.de>
2023-10-18 22:49:03 +0200

    split diffusion_cfg.py

---
+++ diffusion.py
@@ -0,0 +1,381 @@
+__author__ = "Alexander Frotscher"
+__email__ = "alexander.frotscher@student.uni-tuebingen.de"
+
+"""
+This code is based on @dome272 implementation of DDPM's
+https://github.com/dome272/Diffusion-Models-pytorch
+"""
+
+from utils import *
+
+class Diffusion:
+    def __init__(self, noise_steps=1000, img_size=128, device="cuda"):
+        self.noise_steps = noise_steps
+
+        self.beta = self.linear_noise_schedule().to(device)
+        self.alpha = 1.0 - self.beta
+        self.alpha_hat = torch.cumprod(self.alpha, dim=0)
+
+        self.img_size = img_size
+        self.device = device
+
+    def linear_noise_schedule(self):
+        beta_start = 1e-4
+        beta_end = 0.02
+        return torch.linspace(beta_start, beta_end, self.noise_steps)
+
+    def noise_images(self, x, t, coarse=False, pyramid=False):
+        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None]
+        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[
+            :, None, None, None
+        ]
+        if coarse == True:
+            noise = coarse_noise(x.shape[0], x.shape[1], x.device)
+        elif pyramid == True:
+            noise = pyramid_noise_like(x.shape[0], x.shape[1], x.device)
+        else:
+            noise = torch.randn_like(x)
+        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * noise, noise
+
+    def sample_timesteps(self, n):
+        return torch.randint(low=1, high=self.noise_steps, size=(n,))
+
+    def ddpm_mu_t(self, x, predicted_noise, t):
+        alpha = self.alpha[t][:, None, None, None]
+        alpha_hat = self.alpha_hat[t][:, None, None, None]
+        return (
+            1
+            / torch.sqrt(alpha)
+            * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise)
+        )
+
+    def ddpm_mean_t(self, x, t, predicted_noise=None, x_0=None):
+        if predicted_noise == None and x_0 == None:
+            print("Either noise or x_0 have to be given to calculate x_t-1.")
+            exit(1)
+        alpha = self.alpha[t][:, None, None, None]
+        alpha_hat = self.alpha_hat[t][:, None, None, None]
+        beta = self.beta[t][:, None, None, None]
+        alpha_hat_minus_one = self.alpha_hat[t - 1][:, None, None, None]
+        if x_0 == None:
+            pred_x0 = (
+                1 / torch.sqrt(alpha_hat) * x
+                - torch.sqrt((1 - alpha_hat) / (alpha_hat)) * predicted_noise
+            )
+            pred_x0 = pred_x0.clamp(-1, 1)
+            # pred_x0 = clamp_to_spatial_quantile(pred_x0,0.99)
+            x_0 = pred_x0
+        w0 = torch.sqrt(alpha_hat_minus_one) * beta / (1 - alpha_hat)
+        wt = torch.sqrt(alpha) * (1 - alpha_hat_minus_one) / (1 - alpha_hat)
+        return w0 * x_0 + wt * x
+
+    def sample(
+        self, model, n, labels, channels, cfg_scale=3, coarse=False, pyramid=False
+    ):  # cfg scale determines the influence of the conditional model
+        model.eval()
+        with torch.no_grad():
+            if coarse == True:
+                x = coarse_noise(n, channels, self.device)
+            elif pyramid == True:
+                x = pyramid_noise_like(n, channels, self.device)
+            else:
+                x = torch.randn((n, channels, self.img_size, self.img_size)).to(
+                    self.device
+                )
+            for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
+                t = (torch.ones(n) * i).long().to(self.device)
+                predicted_noise = model(x, t)
+                if cfg_scale > 0:
+                    uncond_predicted_noise = model(x, t, None)
+                    predicted_noise = torch.lerp(
+                        uncond_predicted_noise, predicted_noise, cfg_scale
+                    )
+                beta = self.beta[t][:, None, None, None]
+                if i > 1:
+                    if coarse == True:
+                        noise = coarse_noise(n, channels, self.device)
+                    elif pyramid == True:
+                        noise = pyramid_noise_like(n, channels, self.device)
+                    else:
+                        noise = torch.randn_like(x)
+                else:
+                    noise = torch.zeros_like(x)
+                x = self.ddpm_mu_t(x, predicted_noise, t) + torch.sqrt(beta) * noise
+                # x = self.ddpm_mu_t_2(x, predicted_noise, t) + torch.sqrt(beta) * noise
+        model.train()
+        x = (x.clamp(-1, 1) + 1) / 2
+        x = (x * 255).type(torch.uint8)
+        return x
+
+    def dpm_encoder(self, model, images, timestemp=None):
+        if timestemp is None:
+            timestemp = self.noise_steps
+        num_images = images.shape[0]
+        model.eval()
+        with torch.no_grad():
+            # First, sample from the forward process by drawing only once
+            xts = torch.zeros(
+                (
+                    num_images,
+                    timestemp,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            zs = torch.zeros(
+                (
+                    num_images,
+                    timestemp - 1,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            t = (torch.ones(num_images) * timestemp - 1).long().to(self.device)
+            x_t, noise = self.noise_images(images, t)
+            xts[:, timestemp - 1] = x_t
+            for i in tqdm(reversed(range(1, timestemp - 1)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                mean = self.ddpm_mean_t(xts[:, i + 1], t, x_0=images)
+                xts[:, i] = mean
+            xts[:, 0] = images
+
+            for i in tqdm(reversed(range(1, timestemp)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                x_t = xts[:, i]
+                x_tm1 = xts[:, i - 1]
+                predicted_noise = model(x_t, t)
+                mu_t = self.ddpm_mu_t(x_t, predicted_noise, t)
+                z_t = x_tm1 - mu_t
+                zs[:, i - 1] = z_t
+        return zs
+
+    def dpm_inversion(self, model, images, timestemp=None):
+        if timestemp is None:
+            timestemp = self.noise_steps
+        num_images = images.shape[0]
+        model.eval()
+        with torch.no_grad():
+            # First, sample from the forward process
+            xts = torch.zeros(
+                (
+                    num_images,
+                    timestemp,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            zs = torch.zeros(
+                (
+                    num_images,
+                    timestemp - 1,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            for i in tqdm(reversed(range(1, timestemp)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                x_t, noise = self.noise_images(images, t)
+                xts[:, i] = x_t
+            xts[:, 0] = images
+
+            for i in tqdm(reversed(range(1, timestemp)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                x_t = xts[:, i]
+                x_tm1 = xts[:, i - 1]
+                predicted_noise = model(x_t, t)
+                mu_t = self.ddpm_mu_t(x_t, predicted_noise, t)
+                z_t = x_tm1 - mu_t
+                zs[:, i - 1] = z_t
+        return zs
+
+    def dpm_differences(self, model, images, start=100, stop=None, pyramid=False):
+        if stop == None:
+            stop = self.noise_steps
+        if start == 0:
+            start = 1
+        num_images = images.shape[0]
+        model.eval()
+        with torch.no_grad():
+            # First, sample from the forward process
+            xts = torch.zeros(
+                (
+                    num_images,
+                    stop - start,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            zs = torch.zeros(
+                (
+                    num_images,
+                    stop - start,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            for i in tqdm(reversed(range(start, stop)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                x_t, noise = self.noise_images(images, t, pyramid=pyramid)
+                xts[:, i - start] = x_t
+
+            # calculate the differences
+            for i in tqdm(reversed(range(start, stop)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                x_t = xts[:, i - start]
+                predicted_noise = model(x_t, t)
+                mu_t = self.ddpm_mu_t(x_t, predicted_noise, t)
+                mean = self.ddpm_mean_t(x_t, t, x_0=images)
+                # what was supposed to be predicted and what is predicted
+                z_t = (mean - mu_t) ** 2
+                zs[:, i - start] = z_t
+        return zs
+
+    def skip_differences(
+        self, model, images, start=100, stop=None, skip=25, pyramid=False
+    ):
+        if stop is None:
+            stop = self.noise_steps
+        if start == 0:
+            start = 1
+        num_images = images.shape[0]
+        model.eval()
+        with torch.no_grad():
+            # First, sample from the forward process
+            xts = torch.zeros(
+                (
+                    num_images,
+                    stop - start,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            zs = torch.zeros(
+                (
+                    num_images,
+                    int(((stop - start) / skip)),
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            for i in tqdm(reversed(range(start, stop)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                x_t, noise = self.noise_images(images, t, pyramid=pyramid)
+                xts[:, i - start] = x_t
+
+            correct_chain = xts[:, -1]
+            predicted_chain = xts[:, -1]
+
+            for i in tqdm(reversed(range(start, stop)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                predicted_noise = model(predicted_chain, t)
+                predicted_chain = self.ddpm_mu_t(predicted_chain, predicted_noise, t)
+                correct_chain = self.ddpm_mean_t(correct_chain, t, x_0=images)
+                if i % skip == 0 or i == 1:
+                    z_t = (correct_chain - predicted_chain) ** 2
+                    zs[:, int((i - start) / skip)] = z_t
+                    predicted_chain = xts[:, i - start]
+                    correct_chain = xts[:, i - start]
+        return zs
+
+    def differences_noise(self, model, images, start=100, stop=None, pyramid=False):
+        if stop is None:
+            stop = self.noise_steps
+        if start == 0:
+            start = 1
+        num_images = images.shape[0]
+        model.eval()
+        with torch.no_grad():
+            # First, sample from the forward process
+            xts = torch.zeros(
+                (
+                    num_images,
+                    stop - start,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            noises = torch.zeros(
+                (
+                    num_images,
+                    stop - start,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            zs = torch.zeros(
+                (
+                    num_images,
+                    stop - start,
+                    images.shape[1],
+                    images.shape[2],
+                    images.shape[3],
+                )
+            ).to(self.device)
+            for i in tqdm(reversed(range(start, stop)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                x_t, noise = self.noise_images(images, t, pyramid=pyramid)
+                xts[:, i - start] = x_t
+                noises[:, i - start] = noise
+
+            for i in tqdm(reversed(range(start, stop)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                x_t = xts[:, i - start]
+                true_noise = noises[:, i - start]
+                predicted_noise = model(x_t, t)
+                z_t = (predicted_noise - true_noise) ** 2
+                zs[:, i - start] = z_t
+        return zs
+
+    # initial idea of using z_t -> does not work well
+    def guide_restoration(self, model, xts, zs, cfg_scale=1.5, noise_scale=0.5):
+        model.eval()
+        num_steps = xts.shape[1]
+        num_images = zs.shape[0]
+        with torch.no_grad():
+            x = xts[:, -1]
+            for i in tqdm(reversed(range(1, num_steps)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                predicted_noise = model(x, t)
+                if cfg_scale > 0:
+                    sample_noise = model(xts[:, i], t, None)
+                    predicted_noise = torch.lerp(
+                        sample_noise, predicted_noise, cfg_scale
+                    )
+                beta = self.beta[t][:, None, None, None]
+                if i > 1:
+                    noise = torch.randn_like(x)
+                else:
+                    noise = torch.zeros_like(x)
+                x = self.ddpm_mu_t(x, predicted_noise, t) + torch.sqrt(beta) * (
+                    noise_scale * noise + (1 - noise_scale) * zs[:, i - 1]
+                )
+        model.train()
+        return x
+
+    def ano_ddpm(self, model, images, num_steps):
+        model.eval()
+        num_images = images.shape[0]
+        with torch.no_grad():
+            t = (torch.ones(num_images) * num_steps).long().to(self.device)
+            x, noise = self.noise_images(images, t)
+            for i in tqdm(reversed(range(1, num_steps)), position=0):
+                t = (torch.ones(num_images) * i).long().to(self.device)
+                predicted_noise = model(x, t)
+                beta = self.beta[t][:, None, None, None]
+                if i > 1:
+                    noise = torch.randn_like(x)
+                else:
+                    noise = torch.zeros_like(x)
+                x = self.ddpm_mu_t(x, predicted_noise, t) + torch.sqrt(beta) * noise
+        model.train()
+        return x
---
+++ train.py
@@ -0,0 +1,153 @@
+__author__ = "Alexander Frotscher"
+__email__ = "alexander.frotscher@student.uni-tuebingen.de"
+
+"""
+This code is based on @dome272 implementation of DDPM's
+https://github.com/dome272/Diffusion-Models-pytorch
+"""
+
+import argparse
+import copy
+import logging
+
+from accelerate import Accelerator, DistributedDataParallelKwargs
+from torch import optim
+
+import wandb
+from diffusion import *
+from modules import *
+from utils import *
+
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+
+
+def train(args):
+    make_dicts(args.run_name)
+    kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+    accelerator = Accelerator(kwargs_handlers=[kwargs])
+    device = accelerator.device
+    dataloader = Brats21(args, preload=True)
+    steps_per_epoch = int(np.ceil(len(dataloader.dataset) / args.batch_size))
+    number_of_steps = steps_per_epoch * args.epochs
+    model = UNet(
+        c_in=args.channels,
+        c_out=args.channels,
+        device=device,
+        img_size=args.image_size,
+    )
+    optimizer = optim.AdamW(
+        model.parameters(), lr=1
+    )  # scheduler multiplies base lr of optimizer -> lr = 1
+
+    mse = nn.MSELoss()
+    diffusion = Diffusion(img_size=args.image_size, device=device)
+    ema = EMA(0.995)
+    ema_model = copy.deepcopy(model).eval().requires_grad_(False)
+    # wandb.watch(model, log="all")
+
+    if args.train_continue == True:
+        args.target_lr = args.start_lr
+        ckpt = torch.load(args.current_model)
+        model.load_state_dict(ckpt)
+        ckpt = torch.load(args.current_opt)
+        optimizer.load_state_dict(ckpt)
+        ckpt = torch.load(args.current_ema)
+        ema_model.load_state_dict(ckpt)
+        ema_model.eval().requires_grad_(False)
+
+    # model = torch.compile(model)
+    scheduler = LRWarmupCosineDecay(
+        optimizer,
+        int(0.05 * number_of_steps),
+        number_of_steps,
+        args.start_lr,
+        args.target_lr,
+    )
+    model, ema_model, optimizer, scheduler, dataloader = accelerator.prepare(
+        model, ema_model, optimizer, scheduler, dataloader
+    )
+
+    for epoch in range(args.epochs):
+        logging.info(f"Starting epoch {epoch}:")
+        pbar = tqdm(dataloader)
+        for i, (images) in enumerate(pbar):
+            images = (images * 2) - 1  # normalization
+            t = diffusion.sample_timesteps(images.shape[0]).to(device)
+            x_t, noise = diffusion.noise_images(images, t, pyramid=False)
+            # if np.random.random() < 0.1:
+            #    labels = None
+            # predicted_noise = model(x_t, t, labels)
+            predicted_noise = model(x_t, t)
+            loss = mse(noise, predicted_noise)
+
+            optimizer.zero_grad()
+            accelerator.backward(loss)
+            optimizer.step()
+            scheduler.step()
+            ema.step_ema(ema_model, model)
+            wandb.log({"MSE": loss.item()})
+
+        if epoch > 120 and epoch % 8 == 0 and accelerator.is_main_process:
+            my_model = accelerator.unwrap_model(model)
+            my_ema_model = accelerator.unwrap_model(ema_model)
+            # labels = torch.arange(args.num_classes).long().to(device)
+            labels = None
+            n = 5
+            accelerator.save(
+                my_model.state_dict(),
+                os.path.join("models", args.run_name, f"{epoch}_ckpt.pt"),
+            )
+            accelerator.save(
+                optimizer.state_dict(),
+                os.path.join("models", args.run_name, f"{epoch}_optim.pt"),
+            )
+            ema_sampled_images = diffusion.sample(
+                ema_model,
+                n=n,
+                labels=labels,
+                channels=args.channels,
+                cfg_scale=0,
+                pyramid=False,
+            )
+            save_images(
+                ema_sampled_images,
+                os.path.join("results", args.run_name, f"{epoch}_ema.jpg"),
+                mode="L",
+            )
+            accelerator.save(
+                my_ema_model.state_dict(),
+                os.path.join("models", args.run_name, f"{epoch}_ema_ckpt.pt"),
+            )
+            example_images = wandb.Image(upload_images(ema_sampled_images, mode="L"))
+            wandb.log({"EMA-DDPM": example_images})
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.run_name = "Brats128_l1"
+    args.epochs = 233
+    args.batch_size = 128
+    args.image_size = 128
+    args.channels = 4
+    args.dataset_path = (
+        "/mnt/lustre/baumgartner/bkc035/data/BraTS2021/BraTS2021_Training_Data"
+    )
+    args.start_lr = 2e-5
+    args.target_lr = 7e-5
+    args.path_to_csv = "/mnt/lustre/baumgartner/bkc035/data/BraTS2021/scans_train.csv"
+    args.train_continue = False
+    args.current_model = "/mnt/lustre/baumgartner/bkc035/normative-diffusion/models/Brats128_pyramid/320_ckpt.pt"
+    args.current_ema = "/mnt/lustre/baumgartner/bkc035/normative-diffusion/models/Brats128_pyramid/320_ema_ckpt.pt"
+    args.current_opt = "/mnt/lustre/baumgartner/bkc035/normative-diffusion/models/Brats128_pyramid/320_optim.pt"
+    torch.backends.cudnn.benchmark = (
+        True  # additional speed up if input size does not change
+    )
+
+    wandb.init(entity="team-frotscher", project=args.run_name, config=args)
+
+    train(args)
+
+
+if __name__ == "__main__":
+    main()
