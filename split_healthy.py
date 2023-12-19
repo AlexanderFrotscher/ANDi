fdbf24e574ec383e9c9b3de79a78badf46e08dc7
AlexanderFro <alex.frotscher@gmx.de>
2023-12-19 15:46:52 +0100

    combine split_healthy and resize

--- split_healthy.py
+++ split_healthy.py
@@ -1,40 +1,18 @@
 __author__ = "Alexander Frotscher"
 __email__ = "alexander.frotscher@student.uni-tuebingen.de"
 
+import argparse
 import os
-import numpy as np
+
 import nibabel as nib
+import numpy as np
 import pandas as pd
-from tqdm.auto import tqdm
-
-# import torch
-
-
-def preprocess_mask(mask):
-    mask_WT = mask.copy()
-    mask_WT[mask_WT == 1] = 1
-    mask_WT[mask_WT == 2] = 1
-    mask_WT[mask_WT == 4] = 1
-    return mask_WT
-
-
-df = pd.read_csv(
-    "/scratch_local/jkapoor83-4570786/BraTS2021_Training_Data/splits/scans_train.csv"
-)
-# df = pd.read_csv('D:/DokumenteD/Uni/Programme/Python/Deep Learning/data/BraTS21/scans_myimage.csv')
-root_path = "/scratch_local/jkapoor83-4570786/BraTS2021_Training_Data"
-# root_path = 'D:/DokumenteD/Uni/Programme/Python/Deep Learning/data/BraTS21/BraTS21_val'
-os.makedirs(
-    "/scratch_local/jkapoor83-4570786/BraTS2021_Training_Data/healthy_slices",
-    exist_ok=True,
-)
+import torch
+from tqdm import tqdm
+from torchvision import transforms
 
 
 def normalize_volume(images):
-    """
-    This function is adapted from https://github.com/AntanasKascenas/DenoisingAE
-    Normalise the intensity values in each modality by scaling by 99 percentile foreground (nonzero) value.
-    """
     for modality in range(images.shape[0]):
         i_ = images[modality, :, :, :].reshape(-1)
         i_ = i_[i_ > 0]
@@ -43,84 +21,99 @@ def normalize_volume(images):
     return images
 
 
-# )
-# ids = df.loc[:, "BraTS21ID"]
-# healthy_ids = []
-# healthy_slices = []
-# i_ = 0
-# for id in tqdm(ids):
-#     mask_path = os.path.join(root_path, id, id + "_seg.nii.gz")
-#     mask = np.asarray(nib.load(mask_path).dataobj, dtype=int)
-#     img_path = os.path.join(root_path, id, id + "_flair.nii.gz")
-#     img_flair = np.asarray(nib.load(img_path).dataobj, dtype=float)
-#     img_path = os.path.join(root_path, id, id + "_t1.nii.gz")
-#     img_t1 = np.asarray(nib.load(img_path).dataobj, dtype=float)
-#     img_path = os.path.join(root_path, id, id + "_t1ce.nii.gz")
-#     img_t1ce = np.asarray(nib.load(img_path).dataobj, dtype=float)
-#     img_path = os.path.join(root_path, id, id + "_t2.nii.gz")
-#     img_t2 = np.asarray(nib.load(img_path).dataobj, dtype=float)
-#     mask = preprocess_mask(mask)
-#     for i in range(img.shape[2]):
-#         my_slice = img_flair[:, :, i]
-#         my_mask = mask[:, :, i]
-#         # my_slice = torch.tensor(my_slice)
-#         if np.count_nonzero(my_slice) and 1 not in my_mask:
-#             healthy_ids.append(id)
-#             healthy_slices.append(i)
-#             healthy_slices_values = np.stack(
-#                 [img_flair, img_t1, img_t1ce, img_t2], axis=0
-#             )
+def main(args):
+    split_and_save(args)
 
-#     return images
 
-#             np.save(
-#                 f"/scratch_local/jkapoor83-4570786/BraTS2021_Training_Data/healthy_slices/{i_:05d}.npy",
-#                 healthy_slices_values,
-#             )
-#             i_ += 1
-
-ids = df.loc[:, "BraTS21ID"]
-data_types = ["_flair.nii.gz", "_t1.nii.gz", "_t1ce.nii.gz", "_t2.nii.gz"]
-i__ = 0
-with tqdm(ids) as pbar:
+def split_and_save(args):
+    df = pd.read_csv(args.input_file)
+    root_path = args.data_set
+    os.makedirs(f"{args.data_set}/healthy_slices", exist_ok=True)
+    ids = df.loc[:, df.columns[0]]
+    my_transform = transforms.Resize(args.resolution, antialias=True)
+    global_ids = []
+    global_slices = []
+    data_types = ["_flair.nii.gz", "_t1.nii.gz", "_t1ce.nii.gz", "_t2.nii.gz"]
+    pbar = tqdm(ids)
     for id in pbar:
+        my_ids = []
+        healthy_slices = []
+        my_slices = []
         images = []
         mask_path = os.path.join(root_path, id, id + "_seg.nii.gz")
         mask = np.asarray(nib.load(mask_path).dataobj, dtype=int)
         for data_type in data_types:
-            img_path = os.path.join(root_path, id, id + data_type)
+            img_path = os.path.join(args.data_set, id, id + data_type)
             img = np.asarray(nib.load(img_path).dataobj, dtype=float)
             images.append(img)
-
-            img = np.stack(images, axis=0)
-            img = normalize_volume(img.astype(np.float32))
-
+        img = torch.stack([torch.from_numpy(x) for x in images], dim=0)
+        img = normalize_volume(img.float())
         mask[mask >= 1] = 1
-        j__ = 0
         for i in range(img.shape[3]):
             my_slice = img[0, :, :, i]
             my_mask = mask[:, :, i]
-            if np.count_nonzero(my_slice) and 1 not in my_mask:
-                j__ += 1
-
+            if (
+                torch.count_nonzero(my_slice) and 1 not in my_mask
+            ):  # filter out empty and slices containing an anomaly
+                my_ids.append(id)
+                healthy_slices.append(i)
+                if args.save == True:
+                    my_slices.append(img[:, :, :, i])
+        if args.save == True:
+            for patient, slice, data in zip(my_ids, healthy_slices, my_slices):
+                my_data = my_transform(data)
+                my_data = my_data.numpy()
                 np.save(
-                    f"/scratch_local/jkapoor83-4570786/BraTS2021_Training_Data/healthy_slices/{i__:05d}.npy",
-                    img[:, :, :, i],
+                    f"{args.data_set}/healthy_slices/{patient}_{slice}.npy", my_data
                 )
-                i__ += 1
-        pbar.set_description(f"{i__:05d}, shape {img.shape[3]}, healthy {j__}")
+        global_ids.extend(my_ids)
+        global_slices.extend(healthy_slices)
+    healthy_dict = {df.columns[0]: global_ids, "Slice": global_slices}
+    df_healthy = pd.DataFrame(healthy_dict)
+    df_healthy.to_csv(args.output_file, index=False)
 
-print(f"Number of healthy slices: {i__:05d}")
 
-
-# healthy_dict = {"BraTS21ID": healthy_ids, "Slice": healthy_slices}
-# df_healthy = pd.DataFrame(healthy_dict)
-# df_healthy.to_csv(
-#     "/scratch_local/jkapoor83-4570786/BraTS2021_Training_Data/splits/healthy_slices_train.csv",
-#     index=False,
-# )
-
-# df_healthy.to_csv(
-#     "/mnt/qb/macke/jkapoor83/brats_data/BraTS2021_Training_Data/splits/healthy_slices_train.csv",
-#     index=False,
-# )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Split the data into healthy slices.")
+    parser.add_argument(
+        "-d",
+        "--data_set",
+        type=str,
+        required=True,
+        metavar="",
+        help="The folder that contains the MRI-Volumes containing anomalies.",
+    )
+    parser.add_argument(
+        "-i",
+        "--input_file",
+        type=str,
+        required=True,
+        metavar="",
+        help="The .csv that specifies the volumes to split into healthy slices.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_file",
+        type=str,
+        required=True,
+        metavar="",
+        help="The .csv path for the file that specifies the healthy slices.",
+    )
+    parser.add_argument(
+        "-s",
+        "--save",
+        type=bool,
+        default=False,
+        metavar="",
+        help="Flag that decides if the healthy slices slices should be stored in a seperate folder.",
+    )
+    parser.add_argument(
+        "-r",
+        "--resolution",
+        type=int,
+        default=128,
+        metavar="",
+        help="The resolution of the stored slices, if save flag is set to true.",
+    )
+    args = parser.parse_args()
+    main(args)
